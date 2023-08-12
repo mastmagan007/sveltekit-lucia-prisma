c2ed176111c13efb830674c9a365ec38f26bebcc
mastmagan007 <tshanajk@gmail.com>
2023-08-12 22:10:10 +0500

    Coming Soon

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="flex justify-content-center align-items-center ctaMain">
+<div class="cta">
+    <h1 class='text-dark text-5xl'>Coming Soon</h1>
+</div>
+</div>
+
+<style>
+.ctaMain{
+    height: calc(100vh - 75px);
+}
+</style>
--- src/routes/properties/+page.svelte
+++ src/routes/properties/+page.svelte
@@ -44,7 +44,54 @@
 	})
 </script>
 
-<div class="container">
+
+<div class="container-fluid innerPageTopBanner"
+    style="background: url({'lib/images/mainBannerNew-1.jpg'}) center center no-repeat;">
+    <div class="container">
+        <div class="row innerPageTopBannerPadding centered ">
+            <div class="col-md-10">
+                <div class="innerPageTopBannerText propertiesPage">
+                    <h1>Search Homes</h1>
+                    <form id="property-filter-form">
+                
+                        <div class="searchBar">
+
+                          
+                            <div class="form-group flex-grow-1">
+								<input type="search" placeholder="Search..." bind:value={$searchStore.search} />
+                            </div>
+                            <div class="form-group">
+                                <select bind:value={$searchStore.selectedCity}>
+									<option value="">All Cities</option>
+									{#each cityOptions as city}
+									  <option value={city}>{city}</option>
+									{/each}
+								  </select>
+							
+                            </div>
+                            <div class="form-group">
+								{#each $searchStore.filtered as value}
+								<div class="value">
+									<h2>{value.title}</h2>
+									<p>{value.city}</p>
+									<p>Price: ${value.listPrice}</p>
+								</div>
+							{/each}
+								
+                            </div>
+                            <!-- <div class="form-group">
+                                <button class="myBtn" type="submit">Search</button>
+                            </div> -->
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- <div class="container">
 	<h1>Search/Filter</h1>
 	<input type="search" placeholder="Search..." bind:value={$searchStore.search} />
 	<select bind:value={$searchStore.selectedCity}>
@@ -70,4 +117,16 @@
 			<p>Price: ${value.listPrice}</p>
 		</div>
 	{/each}
+</div> -->
+
+<div class="container-fluid">
+    <div class="container">
+
+
+        <div class="row py-3 py-lg-4 py-5">
+
+        </div>
+
+    </div>
 </div>
+
